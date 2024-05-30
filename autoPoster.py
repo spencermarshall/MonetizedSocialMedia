3920ba6fb212946a6cf8febd493ea7c9a014df73
Spencer Marshall <sm892@byu.edu>
2024-05-29 21:10:19 -0500

    stuck on error 401, could not authenticate you 32- perhaps it needs time to load in my developer account, idk really tho, not securr

--- autoPoster.py
+++ autoPoster.py
@@ -2,23 +2,38 @@
 import tweepy
 
 
-#My Credentials, these are fake variables, the real with real values are stored in github secret variables
-API_KEYFake = None
-API_SECRET_KEYFake = None
-ACCESS_TOKENFake = None
-ACCESS_TOKEN_SECRETFake = None
+#My Credentials
+API_KEY = 'FMYNXsMFBMu5SfgOEC0hA2lxF'
+API_SECRET_KEY = 'RVNVnPALx141A5t5PuXkUX7K8tuD5pfO1PYSLoMuKucNH7q8rp'
+access_token = '1773130626134048768-JYUK22qsQwr73cjUh8IILs0pMk8Lqc'
+access_token_secret = 'zBvbWxTjjsoe1WNdwX9GSxD1qQSUDSR0cJi7M0bSg0mfA'
 
+consumer_key = 'os9Yea8yX1HdH4DYcTHwwn4Oi'
+consumer_secret = 'lNbRTOqF7emu1Ojt1zSbA3s2jd3tWvKnDZoQjIL8Bcp8aMXCwp'
 
-# Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFQruAEAAAAAo7wS5V4JvD2z0mm8FQJkDiQa9XE%3DfXM6vBkQtGhRJJWUm02D8i1HcQcHX2lPngEy9FGIYSKjaspKbh'
+
+# client = tweepy.Client(bearer_token=bearer_token,
+#     consumer_key=consumer_key, consumer_secret=consumer_secret,
+#     access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+#auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+#auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+client = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
+
+# Define the tweet text
+tweet_text = "May the force be with you, always."
 
 # Create a tweet
-tweet = "May the force be with you, always"
-
-# Post the tweet
 try:
-    api.update_status(status=tweet)
+    tweet = api.update_status(status=tweet_text)
+
+
+
     print("Tweet posted successfully!")
-except tweepy.TweepError as e:
-    print(f"Error: {e.reason}")
+    print("Tweet ID:", tweet.id)
+    print("Tweet Text:", tweet.text)
+except tweepy.TweepyException as e:
+    print("Error:", e)
