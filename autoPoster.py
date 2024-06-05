e0e4ca494f1e95e8cb88c2a9e35409305e1eca6a
Spencer Marshall <sm892@byu.edu>
2024-06-05 18:22:59 -0500

    it's working

--- autoPoster.py
+++ autoPoster.py
@@ -1,39 +1,28 @@
-#twitter api
 import tweepy
 
+# My Credentials
+API_KEY = '4DvyV5GHRJpd35XZ9dfCXIPPH'
+API_SECRET_KEY = 'SNCCqXHUREJwuYSO6svmlVvbzUYZGyV5yQphuePkhr9N5f8u0c'
+access_token = '1773130626134048768-cYkdSf64H0ketKDLH8E8qwtIcPrtgs'
+access_token_secret = 'paLRa3fJNJRIishcVBCVhohKavqbceylkBRk7YLMv8pQ2'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFQruAEAAAAAkzMGtgA%2BgDcNhES0wLvhp5whth4%3Dq5pBHJwn7Y91cAIdmPIQAVYLWgnyFDerbrYHS2HafXVLg6N7AB'
 
-#My Credentials
-API_KEY = 'FMYNXsMFBMu5SfgOEC0hA2lxF'
-API_SECRET_KEY = 'RVNVnPALx141A5t5PuXkUX7K8tuD5pfO1PYSLoMuKucNH7q8rp'
-access_token = '1773130626134048768-JYUK22qsQwr73cjUh8IILs0pMk8Lqc'
-access_token_secret = 'zBvbWxTjjsoe1WNdwX9GSxD1qQSUDSR0cJi7M0bSg0mfA'
-
-consumer_key = 'os9Yea8yX1HdH4DYcTHwwn4Oi'
-consumer_secret = 'lNbRTOqF7emu1Ojt1zSbA3s2jd3tWvKnDZoQjIL8Bcp8aMXCwp'
-
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFQruAEAAAAAo7wS5V4JvD2z0mm8FQJkDiQa9XE%3DfXM6vBkQtGhRJJWUm02D8i1HcQcHX2lPngEy9FGIYSKjaspKbh'
-
-# client = tweepy.Client(bearer_token=bearer_token,
-#     consumer_key=consumer_key, consumer_secret=consumer_secret,
-#     access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
-#auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-#auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-client = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
+# Initialize the tweepy.Client with the necessary credentials
+client = tweepy.Client(bearer_token=bearer_token,
+                       consumer_key=API_KEY, consumer_secret=API_SECRET_KEY,
+                       access_token=access_token, access_token_secret=access_token_secret)
 
 # Define the tweet text
 tweet_text = "May the force be with you, always."
 
 # Create a tweet
 try:
-    tweet = api.update_status(status=tweet_text)
+    # Post the tweet using the Client
+    response = client.create_tweet(text=tweet_text)
 
-
-
+    # If successful, print out the tweet details
     print("Tweet posted successfully!")
-    print("Tweet ID:", tweet.id)
-    print("Tweet Text:", tweet.text)
+    print("Tweet ID:", response.data['id'])
+    print("Tweet Text:", response.data['text'])
 except tweepy.TweepyException as e:
     print("Error:", e)
