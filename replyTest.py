46b7dd2b5e1588804797a1728619b493e6103bd3
Spencer Marshall <sm892@byu.edu>
2024-06-29 18:30:02 -0500

    fixed back to normal

--- replyTest.py
+++ replyTest.py
@@ -2,14 +2,13 @@ import tweepy
 from dotenv import load_dotenv
 import os
 
-class Reply:
+class TwitterClient:
     def __init__(self):
         load_dotenv()
         bearer_token = os.getenv('BEARER_TOKEN')
         self.client = tweepy.Client(bearer_token=bearer_token)
 
-    def search_tweets_by_username(self, username, max_results=10):
-        query = f"from:{username}"
+    def search_tweets_by_keyword(self, query, max_results=10):
         try:
             tweets = self.client.search_recent_tweets(query=query, max_results=max_results)
             if tweets.data:
@@ -21,5 +20,5 @@ class Reply:
             print(f"Error: {e}")
 
 if __name__ == "__main__":
-    poster = Reply()
-    poster.search_tweets_by_username("TwitterUsername")
+    client = TwitterClient()
+    client.search_tweets_by_keyword("Python", 10)
