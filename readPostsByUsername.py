c7ebab2489c9d8a9b3df7e660bfbc6a54e8ae5b3
Spencer Marshall <sm892@byu.edu>
2024-06-29 20:42:26 -0500

    twrying tweepy to read user timeline, got stuck on 403 error

--- readPostsByUsername.py
+++ readPostsByUsername.py
@@ -1,24 +1,24 @@
+import requests
+import os
+# Replace with your own credentials obtained from your Twitter developer account
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAADGfuAEAAAAAiKXhRiJILcmDv54r2LVG8PpizMU%3DOveJUzcf8xrH2G1uL32SFFG1WAj0s1He8Wf75y4lYUX0jcOj1K'
 
-from apify_client import ApifyClient
+def create_headers(bearer_token):
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+    return headers
 
-# Initialize the ApifyClient with your Apify API token
-client = ApifyClient("apify_api_knYA2rQ4ShCsxomYWWHqwfxizBDga11oF9IY")
+def get_user_info(username):
+    url = f"https://api.twitter.com/2/users/by/username/{username}"
+    headers = create_headers(bearer_token)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code} - {response.json()}")
+        return None
 
-# Prepare the Actor input
-run_input = {
-    "handles": ["realswtheory"],
-    "tweetsDesired": 3,  # Specify the number of tweets you want
-    "proxyConfig": { "useApifyProxy": True },
-}
-
-# Run the Actor and wait for it to finish
-run = client.actor("quacker/twitter-scraper").call(run_input=run_input)
-
-# Fetch and print Actor results from the run's dataset (if there are any)
-print("💾 Check your data here: https://console.apify.com/storage/datasets/" + run["defaultDatasetId"])
-for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-    print("NEXT POST:")
-    print(item['full_text'])
-    print("\n\n")
-
-# 📚 Want to learn more 📖? Go to → https://docs.apify.com/api/client/python/docs/quick-start
+if __name__ == "__main__":
+    username = 'elonmusk'
+    user_info = get_user_info(username)
+    if user_info:
+        print(user_info)
