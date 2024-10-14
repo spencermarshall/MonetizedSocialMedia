733b849446f66136a5c57692ce421d60cc4eff2a
Spencer Marshall <sm892@byu.edu>
2024-10-13 20:56:44 -0600

    moved folder location

--- .StarWars_Poster/aws_sw_video.py
+++ .StarWars_Poster/aws_sw_video.py
@@ -10,7 +10,7 @@ client = tweepy.Client(bearer_token = bearer_token,
 auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, access_token,access_token_secret)
 api = tweepy.API(auth)
 
-video_path = '../videos/2 Ahsoka Short (trailer) 30s - Copy.mp4'
+video_path = 'videos/2 Ahsoka Short (trailer) 30s - Copy.mp4'
 media = api.media_upload(video_path)
 
 client.create_tweet(text="Ahsoka", media_ids=[media.media_id])
