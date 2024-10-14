b7742e21a0c65811ebbf670a6a53987af167b4cd
Spencer Marshall <sm892@byu.edu>
2024-10-13 20:55:56 -0600

    added new SW video folder

--- .StarWars_Poster/aws_sw_video.py
+++ .StarWars_Poster/aws_sw_video.py
@@ -2,7 +2,6 @@ import random
 import tweepy
 
 #insert api keys
-#the rest of this code works for simple mp4 upload assuming mp4 is in my current directory, need to accomondate for aws
 
 client = tweepy.Client(bearer_token = bearer_token,
                                 consumer_key = API_KEY, consumer_secret = API_SECRET_KEY,
@@ -11,7 +10,7 @@ client = tweepy.Client(bearer_token = bearer_token,
 auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, access_token,access_token_secret)
 api = tweepy.API(auth)
 
-video_path = '2 Ahsoka Short (trailer) 30s - Copy.mp4'
+video_path = '../videos/2 Ahsoka Short (trailer) 30s - Copy.mp4'
 media = api.media_upload(video_path)
 
 client.create_tweet(text="Ahsoka", media_ids=[media.media_id])
