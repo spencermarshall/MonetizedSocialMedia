25a5400868ea6230b63367db78c17da7d5ee4f41
Spencer Marshall <sm892@byu.edu>
2024-10-13 22:34:56 -0600

    updated aws_bb_video, pulling from s3 bucket, almost complete.

--- .BreakingBad_Poster/aws_bb_video.py
+++ .BreakingBad_Poster/aws_bb_video.py
@@ -1,34 +1,30 @@
 import boto3 #this is pre-downloaded on aws
 import random
 import tweepy
+import os
 
+def aws_bb_video(event, context):
 
-
-
-
-
-
-def lambda_handler(event, context):
     #AWS api key's
     #...
     #...
     #...
-
-    # Initialize the S3 client
-    s3_client = boto3.client('s3')
-
-    # Define your S3 bucket name
-    bucket_name = 'your-s3-bucket-name'
-
-    # List objects in the bucket
-    response = s3_client.list_objects_v2(Bucket=bucket_name)
-
     client = tweepy.Client(bearer_token=bearer_token,
                            consumer_key=API_KEY, consumer_secret=API_SECRET_KEY,
                            access_token=access_token, access_token_secret=access_token_secret)
 
     auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, access_token, access_token_secret)
     api = tweepy.API(auth)
+
+    # Initialize the S3 client
+    s3_client = boto3.client('s3')
+
+    # Define your S3 bucket name
+    bucket_name = 'bb.videos'   #'s3://bb.videos'
+
+    # List objects in the bucket
+    response = s3_client.list_objects_v2(Bucket=bucket_name)
+
 
     # Check if there are any objects in the bucket
     if 'Contents' not in response:
@@ -50,10 +46,103 @@ def lambda_handler(event, context):
     # Select a random MP4 file
     random_file = random.choice(mp4_files)
 
-    # Return the randomly selected file
+    #filter season and episode
+    s_pos = random_file.find('s') + 1  # The season number starts after 's'
+    e_pos = random_file.find('e') + 1  # The episode number starts after 'e'
+
+    # Extract season and episode numbers
+    season = int(random_file[s_pos:e_pos - 1])  # Convert to integer
+    episode = int(random_file[e_pos:random_file.find(' ', e_pos)])  # Stop at the first space
+    tweet_text = f"Breaking Bad - Season {season} Episode {episode}"
+    se = 's' + str(season) + 'e' + str(episode)
+
+    breaking_bad_episodes = {
+        "s1e1": "Pilot",
+        "s1e2": "Cat's in the Bag...",
+        "s1e3": "...And the Bag's in the River",
+        "s1e4": "Cancer Man",
+        "s1e5": "Gray Matter",
+        "s1e6": "Crazy Handful of Nothin'",
+        "s1e7": "A No-Rough-Stuff-Type Deal",
+
+        "s2e1": "Seven Thirty-Seven",
+        "s2e2": "Grilled",
+        "s2e3": "Bit by a Dead Bee",
+        "s2e4": "Down",
+        "s2e5": "Breakage",
+        "s2e6": "Peekaboo",
+        "s2e7": "Negro y Azul",
+        "s2e8": "Better Call Saul",
+        "s2e9": "4 Days Out",
+        "s2e10": "Over",
+        "s2e11": "Mandala",
+        "s2e12": "Phoenix",
+        "s2e13": "ABQ",
+
+        "s3e1": "No Más",
+        "s3e2": "Caballo sin Nombre",
+        "s3e3": "I.F.T.",
+        "s3e4": "Green Light",
+        "s3e5": "Más",
+        "s3e6": "Sunset",
+        "s3e7": "One Minute",
+        "s3e8": "I See You",
+        "s3e9": "Kafkaesque",
+        "s3e10": "Fly",
+        "s3e11": "Abiquiu",
+        "s3e12": "Half Measures",
+        "s3e13": "Full Measure",
+
+        "s4e1": "Box Cutter",
+        "s4e2": "Thirty-Eight Snub",
+        "s4e3": "Open House",
+        "s4e4": "Bullet Points",
+        "s4e5": "Shotgun",
+        "s4e6": "Cornered",
+        "s4e7": "Problem Dog",
+        "s4e8": "Hermanos",
+        "s4e9": "Bug",
+        "s4e10": "Salud",
+        "s4e11": "Crawl Space",
+        "s4e12": "End Times",
+        "s4e13": "Face Off",
+
+        "s5e1": "Live Free or Die",
+        "s5e2": "Madrigal",
+        "s5e3": "Hazard Pay",
+        "s5e4": "Fifty-One",
+        "s5e5": "Dead Freight",
+        "s5e6": "Buyout",
+        "s5e7": "Say My Name",
+        "s5e8": "Gliding Over All",
+        "s5e9": "Blood Money",
+        "s5e10": "Buried",
+        "s5e11": "Confessions",
+        "s5e12": "Rabid Dog",
+        "s5e13": "To'hajiilee",
+        "s5e14": "Ozymandias",
+        "s5e15": "Granite State",
+        "s5e16": "Felina"
+    }
+
+    episode_title = breaking_bad_episodes[se]
+
+    tweet_text += episode_title
+
+    # Download the random file to /tmp/ in Lambda
+    download_path = f"/tmp/{os.path.basename(random_file)}"
+    s3_client.download_file(bucket_name, random_file, download_path)
+
+    # Upload the file to Twitter using Tweepy
+    media = api.media_upload(download_path)
+
+    title = 'placeholder'
+    # Create a tweet with the uploaded media
+    client.create_tweet(text=tweet_text, media_ids=[media.media_id])
+
     return {
         'statusCode': 200,
-        'body': f'Random MP4 file selected: {random_file}'
+        'body': f"Tweet posted with media: {random_file}"
     }
 
 
