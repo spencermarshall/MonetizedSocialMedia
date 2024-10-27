2b658cced9c0936ac636cf1695b71a055d5e609e
Spencer Marshall <sm892@byu.edu>
2024-10-26 18:22:33 -0600

    made template for aws_lotr_quote

---
+++ .LOTR_Poster/aws_lotr_quote.py
@@ -0,0 +1,25 @@
+import random
+import tweepy
+import os
+
+lotr_api_key = 'placeholder'
+lotr_api_key_secret = 'placeholder'
+lotr_bearer_token = 'placeholder'
+lotr_access_token = 'placeholder'
+lotr_access_token_secret = 'placeholder'
+
+
+client = tweepy.Client(bearer_token = lotr_bearer_token,
+                                consumer_key = lotr_api_key, consumer_secret = lotr_api_key_secret,
+                                access_token = lotr_access_token, access_token_secret = lotr_access_token_secret)
+
+def post_lotr_quote(event, context):
+    lotr_quotes = {
+        0: "\"You're a wizard, Harry.\" -Hagrid",
+    }
+
+    tweet_text = lotr_quotes[random.randint(0,len(lotr_quotes)-1)]
+    #identify if lotr or hobbit for hashtag, might be possible.
+    tweet_text += " #LordOfTheRings" #verify this is best hashtag
+    tweet_text += " #TheHobbit" #verify this is best hashtag
+    client.create_tweet(text=tweet_text)
