39c769a15524356a4f1fc072e9d8d86cefb1d5c8
Spencer Marshall <sm892@byu.edu>
2025-02-12 22:23:22 -0700

    updated sw question

--- .StarWars_Poster/aws_sw_question.py
+++ .StarWars_Poster/aws_sw_question.py
@@ -240,7 +240,7 @@ def SW_question(event, context):
         212: "What are your thoughts on Bill Burr appearing in The Mandalorian",
         213: "What are your thoughts on Hera Syndulla's portrayal in live action?",
         214: "Is the Ahsoka show just Rebels Season 5?",
-        215: "What are your opinions on Jude Law?",
+        215: "What are your opinions on Jod Na Nawood (Jude Law) from Skeleton Crew?",
         216: "What are your thoughts on Omega from The Bad Batch?",
         217: "What are your thoughts on Crosshair from The Bad Batch?",
         218: "What are your thoughts on Hunter from The Bad Batch?",
@@ -263,22 +263,239 @@ def SW_question(event, context):
 
     # s3 bucket files look up
     lookup = {
-        89: "questions/char_Yoda(puppet)/",  # yoda puppet
-        215: "questions/char_JudeLaw/",
-        216: "questions/char_Omega/",
-        217: "questions/char_Crosshair/",
-        218: "questions/char_Hunter/",
-        219: "questions/char_Tech/",
-        220: "questions/char_Wrecker/",
-        221: "questions/char_Echo/",
-        226: "questions/char_Leia(Kenobi-show)/",
-        227: "questions/char_Obi-Wan(Kenobi-Show)/",
-        228: "questions/char_Vader(Kenobi-show)/",
-        229: "questions/char_OwenBeruLars(Kenobi-show)/",
+        1: "None",
+        2: "None",
+        3: "None",
+        4: "None",
+        5: "None",
+        6: "None",
+        7: "None",
+        8: "None",
+        9: "None",
+        10: "None",
+        11: "None",
+        12: "None",
+        13: "None",
+        14: "None",
+        15: "None",
+        16: "None",
+        17: "None",
+        18: "None",
+        19: "None",
+        20: "None",
+        21: "None",
+        22: "None",
+        23: "None",
+        24: "None",
+        25: "None",
+        26: "None",
+        27: "None",
+        28: "None",
+        29: "None",
+        30: "None",
+        31: "None",
+        32: "None",
+        33: "None",
+        34: "None",
+        35: "None",
+        36: "None",
+        37: "None",
+        38: "None",
+        39: "None",
+        40: "None",
+        41: "None",
+        42: "None",
+        43: "None",
+        44: "None",
+        45: "None",
+        46: "None",
+        47: "None",
+        48: "None",
+        49: "None",
+        50: "None",
+        51: "None",
+        52: "None",
+        53: "None",
+        54: "None",
+        55: "None",
+        56: "None",
+        57: "None",
+        58: "None",
+        59: "None",
+        60: "None",
+        61: "None",
+        62: "None",
+        63: "None",
+        64: "None",
+        65: "None",
+        66: "None",
+        67: "None",
+        68: "None",
+        69: "None",
+        70: "None",
+        71: "None",
+        72: "None",
+        73: "None",
+        74: "None",
+        75: "None",
+        76: "None",
+        77: "None",
+        78: "None",
+        79: "None",
+        80: "None",
+        81: "None",
+        82: "None",
+        83: "None",
+        84: "None",
+        85: "None",
+        86: "None",
+        87: "None",
+        88: "None",
+        89: "None",  # "questions/char_Yoda(puppet)/", #yoda puppet
+        90: "None",
+        91: "None",
+        92: "None",
+        93: "None",
+        94: "None",
+        95: "None",
+        96: "None",
+        97: "None",
+        98: "None",
+        99: "None",
+        100: "None",
+        101: "None",
+        102: "None",
+        103: "None",
+        104: "None",
+        105: "None",
+        106: "None",
+        107: "None",
+        108: "None",
+        109: "None",
+        110: "None",
+        111: "None",
+        112: "None",
+        113: "None",
+        114: "None",
+        115: "None",
+        116: "None",
+        117: "None",
+        118: "None",
+        119: "None",
+        120: "None",
+        121: "None",
+        122: "None",
+        123: "None",
+        124: "None",
+        125: "None",
+        126: "None",
+        127: "None",
+        128: "None",
+        129: "None",
+        130: "None",
+        131: "None",
+        132: "None",
+        133: "None",
+        134: "None",
+        135: "None",
+        136: "None",
+        137: "None",
+        138: "None",
+        139: "None",
+        140: "None",
+        141: "None",
+        142: "None",
+        143: "None",
+        144: "None",
+        145: "None",
+        146: "None",
+        147: "None",
+        148: "None",
+        149: "None",
+        150: "None",
+        151: "None",
+        152: "None",
+        153: "None",
+        154: "None",
+        155: "None",
+        156: "None",
+        157: "None",
+        158: "None",
+        159: "None",
+        160: "None",
+        161: "None",
+        162: "None",
+        163: "None",
+        164: "None",
+        165: "None",
+        166: "None",
+        167: "None",
+        168: "None",
+        169: "None",
+        170: "None",
+        171: "None",
+        172: "None",
+        173: "None",
+        174: "None",
+        175: "None",
+        176: "None",
+        177: "None",
+        178: "None",
+        179: "None",
+        180: "None",
+        181: "None",
+        182: "None",
+        183: "None",
+        184: "None",
+        185: "None",
+        186: "None",
+        187: "None",
+        188: "None",
+        189: "None",
+        190: "None",
+        191: "None",
+        192: "None",
+        193: "None",
+        194: "None",
+        195: "None",
+        196: "None",
+        197: "None",
+        198: "None",
+        199: "None",
+        200: "None",
+        201: "None",
+        202: "None",
+        203: "None",
+        204: "None",
+        205: "None",
+        206: "None",
+        207: "None",
+        208: "None",
+        209: "None",
+        210: "None",
+        211: "None",
+        212: "None",
+        213: "None",
+        214: "None",
+        215: "None",  # "questions/char_JudeLaw/",
+        216: "None",  # "questions/char_Omega/",
+        217: "None",  # "questions/char_Crosshair/",
+        218: "None",  # "questions/char_Hunter/",
+        219: "None",  # "questions/char_Tech/",
+        220: "None",  # "questions/char_Wrecker/",
+        221: "None",  # "questions/char_Echo/",
+        222: "None",
+        223: "None",
+        224: "None",
+        225: "None",
+        226: "None",  # "questions/char_Leia(Kenobi-show)/",
+        227: "None",  # "questions/char_Obi-Wan(Kenobi-Show)/",
+        228: "None",  # "questions/char_Vader(Kenobi-show)/",
+        229: "None",  # "questions/char_OwenBeruLars(Kenobi-show)/",
         230: "None",
-        231: "questions/char_SenatorOrgana(Kenobi-Show)/",
-        232: "questions/char_Kumail(Kenobi-Show)/",
-        233: "questions/char_DarthJarJar/"
+        231: "None",  # "questions/char_SenatorOrgana(Kenobi-Show)/",
+        232: "None",  # "questions/char_Kumail(Kenobi-Show)/",
+        233: "None"  # "questions/char_DarthJarJar/"
     }
 
     bucket_name = 'starwars.photos'
@@ -300,57 +517,45 @@ def SW_question(event, context):
         while index in question_indices:
             index = random.randint(1, len(questions))
 
-        # gets image from s3 bucket, not yet implemented
-        # temp = lookup[index]
-        # response = s3.list_objects_v2(Bucket=bucket_name, Prefix=temp)
-        # if 'Contents' not in response:
-        #     return {
-        #         'statusCode': 404,
-        #         'body': 'No files found in the specified directory.'
-        #     }
-        # # Extract file keys (paths) from the response
-        # file_keys = [obj['Key'] for obj in response['Contents'] if obj['Key'] != temp]
-        # # Check if there are files to choose from
-        # if not file_keys:
-        #     return {
-        #         'statusCode': 404,
-        #         'body': 'No files found in the specified directory.'
-        #     }
-        # # Pick a random file from the list
-        # random_file = random.choice(file_keys)
+        path = lookup[index]
 
         question_indices.insert(0, index)
-
-        # Ensure the list does not exceed its original length
         if len(question_indices) > 100:
             question_indices.pop()  # Remove the last element
 
-        # Convert the updated list back to JSON string
         updated_content = json.dumps(question_indices)
 
-        # Upload the updated content back to S3
         s3.put_object(Bucket=bucket_name, Key=file_key, Body=updated_content)
         question = questions[index]
 
-        client.create_tweet(text=question)
-        # these 4 are for image, not implemented yet:   TODO
-        # download_path = f"/tmp/{os.path.basename(random_file)}"
-        # s3.download_file(bucket_name, random_file, download_path)
-        # media = api.media_upload(download_path)
-        # client.create_tweet(text=question, media_ids=[media.media_id])
+        if path == "None":  # upload just text, no image
+            client.create_tweet(text=question)
+            return f"POSTED TWEET {question} with no image"
 
-        return {
-            'statusCode': 200,
-            'body': f"Updated SW_questions.txt with new index {index}."
-        }
+        response = s3.list_objects_v2(Bucket=bucket_name, Prefix=path)
+        if 'Contents' not in response:
+            return {
+                'statusCode': 404,
+                'body': 'No files found in the specified directory.'
+            }
+        # Extract file keys (paths) from the response
+        file_keys = [obj['Key'] for obj in response['Contents'] if obj['Key'] != path]
+        # Check if there are files to choose from
+        if not file_keys:
+            return {
+                'statusCode': 404,
+                'body': 'No files found in the specified directory.'
+            }
 
+        # Pick a random file from the list
+        random_file = random.choice(file_keys)
 
+        # these 4 are for image, not implemented yet:
+        download_path = f"/tmp/{os.path.basename(random_file)}"
+        s3.download_file(bucket_name, random_file, download_path)
+        media = api.media_upload(download_path)
+        client.create_tweet(text=question, media_ids=[media.media_id])
+        return f"tweeted image with question {question}"
     except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': f"An error occurred: {str(e)}"
-        }
+        print(e)
 
-    # post tweet question
-
-    # update 50 most recent dynamo DB, FIFO so it keeps track of 50 most recent questions/id
