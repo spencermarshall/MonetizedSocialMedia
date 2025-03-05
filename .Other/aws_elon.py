cf93336d2ef3932b3a9125bb70ec81a14d3f0907
Spencer Marshall <sm892@byu.edu>
2025-03-05 10:39:08 -0700

    updated aws daily

--- .Other/aws_elon.py
+++ .Other/aws_elon.py
@@ -20,24 +20,21 @@ def elon_musk_daily(event, context):
     ran = random.random()
     if ran < 0.01:
         client.create_tweet(text="No")
+    elif ran < 0.011:
+        client.create_tweet(text="Nope")
+    elif ran < 0.012:
+        client.create_tweet(text="Not today")
 
     s3 = boto3.client('s3')
-
-    # Define the bucket and key based on your S3 URL: s3://aws.misc/el.jpeg
     bucket_name = "aws.misc"
     key = "el.jpeg"
 
-    # Use a temporary local file path for the downloaded file
     local_filename = "/tmp/el.jpeg"
 
-    # Download the file from S3
     s3.download_file(bucket_name, key, local_filename)
 
-    # Upload the downloaded file to Twitter as media
     media = api.media_upload(local_filename)
 
-    # Define your tweet text
     tweet_text = 'In a tragic turn of events, Elon Musk has been found alive in his home in Austin, Texas. He was 54. \n\n(Via @NYPOST)'
 
-    # Create a tweet with the uploaded media
     client.create_tweet(text=tweet_text, media_ids=[media.media_id])
