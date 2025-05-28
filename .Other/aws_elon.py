f043c4651e84620468f18a95a9aa2080b914e55a
Spencer Marshall <sm892@byu.edu>
2025-05-28 16:53:36 -0600

    updated el

--- .Other/aws_elon.py
+++ .Other/aws_elon.py
@@ -7,32 +7,63 @@ import boto3
 import requests
 from datetime import datetime, timedelta
 
-client = tweepy.Client(bearer_token=bearer_token,
-                       consumer_key=api_key, consumer_secret=api_key_secret,
-                       access_token=access_token, access_token_secret=access_token_secret)
 
-auth = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)
+
+client = tweepy.Client(
+    bearer_token=bearer_token,
+    consumer_key=api_key,
+    consumer_secret=api_key_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
+
+auth = tweepy.OAuth1UserHandler(
+    api_key, api_key_secret,
+    access_token, access_token_secret
+)
 api = tweepy.API(auth)
 
+# --- Initialize S3 client ---
+s3_client = boto3.client('s3')
+BUCKET_NAME = 'elon.media'
 
 def elon_musk_daily(event, context):
-    text = ''
-    ran = random.randint(1, 2)
-    if ran == 1:
-        text = 'No'
-    elif ran == 2:
-        negative_words = ['Nope', 'Not today', 'Negative', 'Not yet', 'Nah', 'Nay', 'Definitely not']
-        text = random.choice(negative_words)
+    # 1) Generate a “negative” text
+    negative_base = ['No', 'Nope', 'Not today', 'Negative', 'Not yet', 'Nah', 'Nay', 'Definitely not']
+    text = random.choice(negative_base)
+    if random.random() < 0.5:
+        text += '!'
 
-    if random.random() < 0.5:  # 50% add '!'
-        text = text + '!'
+    # 2) Decide branch: 50% text-only, 50% text+image
+    if random.random() < 0.5:
+        # Text-only tweet
+        client.create_tweet(text=text)
+    else:
+        # Image branch
+        # 2a) List and filter image files in S3
+        resp = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
+        keys = [
+            obj['Key'] for obj in resp.get('Contents', [])
+            if obj['Key'].lower().endswith(('.jpg', '.jpeg', '.png', '.webp', '.gif'))
+        ]
 
-    # next add images from s3 bucket 50% of time
-    client.create_tweet(text=text)
+        if not keys:
+            # Fallback to text-only if no images found
+            print("Couldn't find image")
+            client.create_tweet(text=text)
+        else:
+            # 2b) Pick a random image
+            key = random.choice(keys)
+            local_path = os.path.join('/tmp', os.path.basename(key))
+
+            # 2c) Download it
+            s3_client.download_file(BUCKET_NAME, key, local_path)
+
+            # 2d) Upload to Twitter and get media_id
+            media = api.media_upload(local_path)
+            media_id = media.media_id
+
+            # 2e) Tweet with image
+            client.create_tweet(text=text, media_ids=[media_id])
+
     return text
-
-    # else add image
-
-    # tweet_text = 'In a tragic turn of events, Elon Musk has been found alive in his home in Austin, Texas. He was 54. \n\n(Via @NYPOST)'
-
-    # client.create_tweet(text=tweet_text, media_ids=[media.media_id])
