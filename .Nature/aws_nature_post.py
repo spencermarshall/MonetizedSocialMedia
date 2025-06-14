688a0b232fc5bca63f14b5dfd5466b73bd2bb96b
Spencer Marshall <sm892@byu.edu>
2025-06-14 13:35:49 -0600

    upadted nature post

---
+++ .Nature/aws_nature_post.py
@@ -0,0 +1,67 @@
+import boto3  # this is pre-downloaded on aws
+import random
+import tweepy
+import os
+
+# X credentials stored in env variables
+API_KEY            = os.environ["API_KEY"]
+API_SECRET_KEY     = os.environ["API_SECRET_KEY"]
+ACCESS_TOKEN       = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET= os.environ["ACCESS_TOKEN_SECRET"]
+BEARER_TOKEN       = os.environ["BEARER_TOKEN"]
+
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api  = tweepy.API(auth)
+
+client = tweepy.Client(
+    bearer_token=BEARER_TOKEN,
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET_KEY,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
+)
+
+s3_client  = boto3.client('s3')
+bucket_name = 'nature.photos'
+
+def NaturePost(event, context):
+    # 1. List up to 1,000 objects in the bucket
+    response = s3_client.list_objects_v2(Bucket=bucket_name)
+
+    if 'Contents' not in response:
+        return {
+            'statusCode': 404,
+            'body': 'No files found in the S3 bucket.'
+        }
+
+    # 2. Filter for root-level image files with our allowed extensions
+    allowed_ext = ('.jpg', '.jpeg', '.png', '.webp')
+    image_keys = [
+        obj['Key']
+        for obj in response['Contents']
+        if obj['Key'].lower().endswith(allowed_ext)
+           and '/' not in obj['Key']   # exclude any “folders”
+    ]
+
+    if not image_keys:
+        return {
+            'statusCode': 404,
+            'body': 'No matching image files found at the root of the bucket.'
+        }
+
+    # 3. Pick one at random
+    random_file = random.choice(image_keys)
+
+
+    # 5. Download from S3 into Lambda’s /tmp
+    download_path = f"/tmp/{os.path.basename(random_file)}"
+    s3_client.download_file(bucket_name, random_file, download_path)
+
+    # 6. Upload media and post tweet
+    media = api.media_upload(download_path)
+    client.create_tweet(text="", media_ids=[media.media_id])
+
+    return {
+        'statusCode': 200,
+        'body': f"Tweet posted with media: {random_file}"
+    }
